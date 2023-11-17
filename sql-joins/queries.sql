2ee3699572dca930c39a110d5abbc1c019ea7d29
samiesmilz <samiesmilz@gmail.com>
2023-11-17 16:55:28 +0800

    Update queries.sql

--- sql-joins/queries.sql
+++ sql-joins/queries.sql
@@ -15,14 +15,9 @@ ORDER BY first_name;
 
 -- Count the number of cars for each owner and display the average price for each of the cars as integers. Display the owners first_name , last_name, average price and count of vehicles. The first_name should be ordered in descending order. Only display results with more than one vehicle and an average price greater than 10000. --
 
-SELECT 
-  first_name, last_name, 
-  ROUND(AVG(price)) as average_price, 
-  COUNT(owner_id) 
-FROM owners o 
-JOIN vehicles v on o.id=v.owner_id 
-GROUP BY 
-  (first_name, last_name) 
-HAVING 
-  COUNT(owner_id) > 1 AND ROUND(AVG(price)) > 10000 
-ORDER BY first_name DESC;
+SELECT o.first_name, o.last_name, ROUND(AVG(v.price)), COUNT(v.owner_id)
+FROM owners o
+JOIN vehicles v ON o.id = v.owner_id
+GROUP BY o.first_name, o.last_name
+HAVING COUNT(v.owner_id) > 1 AND ROUND(AVG(v.price)) > 10000
+ORDER BY o.first_name DESC;
